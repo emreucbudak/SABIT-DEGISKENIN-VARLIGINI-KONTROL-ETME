857e161e4d97c23a0c057fdd7161560cec1e1c74
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-25 21:35:51 +0300

    Add files via upload

---
+++ main (7).php
@@ -0,0 +1,10 @@
+<?php
+/******************************************************************************
+
+Emre Üçbudak Karadeniz Teknik Üniveristesi
+
+*******************************************************************************/
+// Sabit Değişkenin varlığını kontrol etme
+define("ad","Emre");
+echo defined("ad");
+?>
